e93a870a337e77822ef63e995dcbe124f1ed8d7b
Biringanine Destin <biringaninedestin@gmail.com>
2023-12-16 20:45:16 +0100

    creating superuser

--- main/settings.py
+++ main/settings.py
@@ -94,7 +94,7 @@ USE_TZ = True
 
 
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 STATIC_URL = 'static/'
