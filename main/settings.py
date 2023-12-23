7537e6749a8806070d21daa2a5a70dd33af31120
Biringanine Destin <biringaninedestin@gmail.com>
2023-12-24 00:49:16 +0100

    adjusting statics files

--- main/settings.py
+++ main/settings.py
@@ -95,13 +95,13 @@ USE_TZ = True
 
 
 # STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/'),]
 
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media/'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
