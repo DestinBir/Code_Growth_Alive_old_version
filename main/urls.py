1f41f2009747256082ec438c8d0cc5ee4348a89e
Biringanine Destin <biringaninedestin@gmail.com>
2023-12-27 18:54:17 +0100

    adding image in team view

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path
 
 from general.views import *
 from blog.views import *
+from main import settings
 from user.views import *
 
 urlpatterns = [
@@ -11,4 +13,5 @@ urlpatterns = [
     path('services', service_view, name='services' ),
     path('about', about_view, name='about-us'),
     path('contact', contact_view, name='contact-us'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- user/views.py
+++ user/views.py
@@ -3,6 +3,6 @@ from .models import *
 
 def about_view(request):
 
-    users = User.objects.filter(status = 'Team')
+    users = User.objects.filter(status = 'team' or 'admin' or 'core')
 
     return render(request, 'user/about-us.html', {'users': users})
