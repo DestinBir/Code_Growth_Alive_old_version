4f633c8e41051ae5a1e1944d524d63c07377bdac
Biringanine Destin <biringaninedestin@gmail.com>
2024-01-21 20:48:08 +0100

    debut modification user authetication

--- main/urls.py
+++ main/urls.py
@@ -9,11 +9,12 @@ from blog.views import *
 from user.views import *
 
 urlpatterns = [
-    path("accounts/", include("django.contrib.auth.urls")),
     path("admin", admin.site.urls),
     path("", home_view, name="home"),
     path("services", service_view, name="services"),
     path("about", about_view, name="about-us"),
     path("contact", contact_view, name="contact-us"),
     path("articles", articles, name="articles"),
+    path("login", login_user, name="login"),
+    path("logout", logout_user, name="logout"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- user/views.py
+++ user/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from .models import *
 
+def login_user(request):
+    error = ''
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('frontPage')
+        else:
+            error = "Le mot de passe ou le nom d'utilisateur est incorrect"
+
+    return render(request, 'core/login.html', {'error':error})
 
 def about_view(request):
     users = Admin.objects.all() 
 
     return render(request, "user/about-us.html", {"users": users})
+
