553a34889a67dfeafa34d1c056a977ed96596a70
Biringanine Destin <biringaninedestin@gmail.com>
2024-01-23 16:05:13 +0200

    one article view and url

--- blog/views.py
+++ blog/views.py
@@ -9,3 +9,8 @@ def articles(request):
     articles = Article.objects.all()
     
     return render(request, "blog/articles.html", {'arts':articles})
+
+def article(request, id):
+    art = Article.objects.get(id=id)
+
+    return render(request, 'blog/article.html', {'art':art})
