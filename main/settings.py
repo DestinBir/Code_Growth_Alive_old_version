bd1c3ac795f805ddedb13204aa583314b85ba537
Biringanine Destin <113047575+DestinBir@users.noreply.github.com>
2024-03-01 23:35:12 +0200

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -83,20 +83,6 @@ DATABASES = {
     }
 }
 
-# To use Neon with Django, you have to create a Project on Neon and specify the project connection settings in your settings.py in the same way as for standalone Postgres.
-
-'''DATABASES = {
-  'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': 'neondb',
-    'USER': 'DestinBir',
-    'PASSWORD': 'HKO9qAUfuk0x',
-    'HOST': 'ep-wild-pond-a5d5rm7a.us-east-2.aws.neon.tech',
-    'PORT': '5432',
-    'OPTIONS': {'sslmode': 'require'},
-  }
-}'''
-
 # adding a redis cache system
 
 '''
